002685fff7d49d25ab4603a79a9c44c4bf272caf
unknown <sauvole75@gmail.com>
2021-07-13 11:49:33 +0700

    add

---
+++ src/Mylist.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+public class Mylist<E> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int size = 0;
+    private Object[] elements = new Object[DEFAULT_CAPACITY];
+    public Mylist(){
+
+    }
+
+    private void ensureCapa() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public void add(E e) {
+        if (size == elements.length) {
+            ensureCapa();
+        }
+        elements[size++] = e;
+    }
+
+    public E get(int i) {
+        if (i>= size || i <0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i );
+        }
+        return (E) elements[i];
+    }
+}
